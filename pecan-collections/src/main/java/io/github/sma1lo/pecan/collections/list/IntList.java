dfde4fd7946dc9a01a4d7a1309fb9244becfaefd
Sma1lo <heee58411@gmail.com>
2026-08-04 15:26:50 +0300

    Initial commit

---
+++ pecan-collections/src/main/java/io/github/sma1lo/pecan/collections/list/IntList.java
@@ -0,0 +1,36 @@
+package collections.list;
+
+public class IntList {
+    private int[] elements;
+    private int size;
+
+    public IntList(int initialCapacity) {
+        elements = new int[initialCapacity];
+    }
+
+    public void add(int value) {
+        if (size == elements.length) {
+            grow();
+        }
+
+        elements[size++] = value;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(index);
+        }
+
+        return elements[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void grow() {
+        int[] newElements = new int[elements.length * 2];
+        System.arraycopy(elements, 0, newElements, 0, elements.length);
+        elements = newElements;
+    }
+}
