df5cebcac60cd4cd80f034dcc872f4b468ede2a3
Sma1lo <heee58411@gmail.com>
2026-08-04 22:12:13 +0300

    feat: add CharList

---
+++ pecan-collections/src/main/java/io/github/sma1lo/pecan/collections/list/CharList.java
@@ -0,0 +1,73 @@
+package io.github.sma1lo.pecan.collections.list;
+
+public class CharList {
+    private char[] elements;
+    private int size;
+
+    public CharList(int initialCapacity) {
+        if (initialCapacity < 0) {
+            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+        }
+
+        elements = new char[initialCapacity];
+
+    }
+
+    public CharList() {
+        this(10);
+    }
+
+    public void add(char value) {
+        if (size == elements.length) {
+            grow();
+        }
+
+        elements[size++] = value;
+    }
+
+    public boolean remove(char value) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == value) {
+                int moved = size - i - 1;
+
+                if (moved > 0) {
+                    System.arraycopy(elements, i + 1, elements, i, moved);
+                }
+
+                size--;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public char get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(index);
+        }
+
+        return elements[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+        size = 0;
+    }
+
+    private void grow() {
+        int newCapacity = elements.length == 0 ? 10 : elements.length * 2;
+
+        char[] newElements = new char[newCapacity];
+        System.arraycopy(elements, 0, newElements, 0, size);
+
+        elements = newElements;
+    }
+}
